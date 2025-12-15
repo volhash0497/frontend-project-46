453bc6b9b859700da0dffaced141aadac617b182
Olga Shyshkina <v.shyshkina97@gmail.com>
2025-12-15 22:42:06 +0100

    third step. add readFileSync and parse options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import getFile from '../src/parseData.js';
 const program = new Command();
 
 program
-  .name('gendiff')
-  //.description('CLI to some JavaScript string utilities')
-  .version('1.0.0');
+    .name('gendiff')
+    .description('CLI for files')
+    .version('1.0.0');
 
 program
-  .description(' Compares two configuration files and shows a difference.')
-  //.argument('<string>', 'string to split')
-.arguments('<filepath1> <filepath2>')
+    .description(' Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format [type]', 'output format')
-  /**.action((str, options) => {
-    //const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });**/
+  .action((filepath1, filepath2) => {
+    
+    const [data1, data2] = getFile(filepath1, filepath2);
+    console.log(data1);
+    console.log(data2);
+
+  });
 
 program.parse(process.argv);
-//const genDiff = () => {
+const genDiff = () => {
 
-//}
+}
 
-//export default genDiff;
+export default genDiff;
