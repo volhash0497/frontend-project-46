843d7a566cae557380a588ced9f3bf5dd52c57b0
Olga Shyshkina <v.shyshkina97@gmail.com>
2025-12-25 17:28:42 +0100

    fourth step: add genDiff function.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,56 @@
 import { Command } from 'commander';
 import getFile from '../src/parseData.js';
 const program = new Command();
+import _ from 'lodash';
+
+const format = (obj) => {
+    const lines = [];
+
+    for (const key in obj) {
+        const valueObj = obj[key];
+        if (Array.isArray(valueObj)) {
+            for (const change of valueObj) {
+                lines.push(`${change.flag} ${key}: ${change.value}`);
+            }
+        } else {
+            lines.push(`${valueObj.flag} ${key}: ${valueObj.value}`);
+    }
+}
+
+  return `{\n  ${lines.join('\n  ')}\n}`;
+}
+
+const genDiff = (data1, data2) => {
+    const keys1 = Object.keys(data1).sort();
+    const keys2 = Object.keys(data2).sort();
+    const keys = _.union(keys1, keys2);
+
+    const result = {};
+    for (const key of keys) {
+        if (_.has(data1, key) !== _.has(data2, key)) {    
+            if (_.has(data1, key)) {
+                const newValue = data1[key];
+                result[key] = {flag: '-', value: newValue}; 
+            } else {
+                const newValue = data2[key];
+                result[key] = {flag: '+', value: newValue};
+            }   
+        }
+
+        if (_.has(data1, key) === _.has(data2, key)) {
+            if (data1[key] === data2[key]) {
+                result[key] = {flag: ' ', value: data1[key]};
+            } else {
+                result[key] = [
+                    {flag: '-', value: data1[key]},
+                    {flag: '+', value: data2[key]}
+                ];
+            }
+        }  
+    }
+    
+    return format(result);
+}
 
 program
     .name('gendiff')
@@ -15,14 +65,15 @@ program
   .action((filepath1, filepath2) => {
     
     const [data1, data2] = getFile(filepath1, filepath2);
-    console.log(data1);
-    console.log(data2);
+    console.log(data1)
+    console.log(data2)
+    const result = genDiff(data1, data2)
+    console.log(result);
 
   });
 
 program.parse(process.argv);
-const genDiff = () => {
 
-}
+
 
 export default genDiff;
